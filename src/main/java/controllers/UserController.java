cc74bdffadfd862e11ae4ac8eb1e1ca5142bf0d2
NourhanD <nourhandarwish002@gmail.com>
2024-05-01 19:25:46 +0300

    correct path

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,5 +1,22 @@
 package controllers;
 
-public class UserController {
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+@Stateless
+@Path("user")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserController {
+	
+	@GET
+	@Path("test")
+	public String getText()
+	{
+		return "hello";
+	}
 }
